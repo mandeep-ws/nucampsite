f4d70351d46dce0d2418f04062033bc0a8e7c288
Mandeep Kumar <kumar.26.mandeep@gmail.com>
2020-02-22 11:59:31 -0800

    week 4 workshop

--- js/scripts.js
+++ js/scripts.js
@@ -15,4 +15,16 @@ $(function () {
         });
 
 
-        
+ $(function(){
+            $("#reserveButton").click(function(){
+                $("#reserveModal").modal("show");
+            })
+
+
+ })    
+ 
+ $(function() {
+   $("#loginButton").click(function() {
+     $("#loginModal").modal("show");
+   });
+ });
